aa3316cac97c9fd37759c48378e7cbcf1c54e87f
Vicente Rodriguez Alfaro <87551025+el-vigesimo-vicho@users.noreply.github.com>
2023-11-05 14:07:21 -0300

    Update gen.cpp
    params: ./chato.exe textito.txt 10000 OSTENTE 2 7
    1 - nombre ejecutable = chato.exe
    2 - nombre archivo a crear = textito.txt
    3 - numero de caracteres de archivo a crear = 10000
    4 - patron a insertar en este archivo= OSTENTE
    5 - numero de repeticiones de patron en archivo a crear = 2
    6 - numero que indica sector donde estara (en este caso en el 7mo 10% de texto) = 7

--- gen.cpp
+++ gen.cpp
@@ -1,54 +1,128 @@
 #include <iostream>
 #include <fstream>
-#include <string>
-#include <ctime>
 #include <cstdlib>
+#include <ctime>
+#include <string>
 
-void generarArchivoConRepeticiones(std::string nombreArchivo, int n, std::string x, int y) {
-    std::ofstream archivo(nombreArchivo);
+using namespace std;
 
-    if (!archivo.is_open()) {
-        std::cerr << "No se pudo abrir el archivo." << std::endl;
-        return;
-    }
-
-    std::string caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    int longitudCaracteres = caracteres.length();
-    int longitudX = x.length();
-    std::srand(static_cast<unsigned>(std::time(nullptr)));
-
-    for (int i = 0; i < n; i++) {
-        // Agregar salto de línea aleatorio
-        if (std::rand() % 10 == 0) {
-            archivo << '\n';
-        } else {
-            char caracterAleatorio = caracteres[std::rand() % longitudCaracteres];
-            archivo << caracterAleatorio;
-        }
-
-        // Randomizar la posición del patrón
-        if (i % (n / y) == 0 && (i + longitudX) <= n) {
-            archivo << x;
-            i += longitudX - 1;
-        }
-    }
-
-    archivo.close();
-    std::cout << "Archivo generado: " << nombreArchivo << std::endl;
-}
-
-int main(int argc, char* argv[]) {
-    if (argc != 5) {
-        std::cerr << "Uso: " << argv[0] << " <nombre_archivo> <longitud_texto> <cadena> <repeticiones>" << std::endl;
+int main(int argc, char *argv[]) {
+    if (argc != 6) {
+        cerr << "Uso: " << argv[0] << " <nombreArchivo> <longitudDeseada> <patronAInsertar> <repeticiones> <sector>" << endl;
         return 1;
     }
 
-    std::string nombreArchivo = argv[1];
-    int longitudTexto = std::stoi(argv[2]);
-    std::string cadena = argv[3];
-    int repeticiones = std::stoi(argv[4]);
 
-    generarArchivoConRepeticiones(nombreArchivo, longitudTexto, cadena, repeticiones);
+    string nombreArchivo = argv[1];
+    int longitudDeseada = atoi(argv[2]);
+    string patronAInsertar = argv[3];
+    int repeticiones = atoi(argv[4]);
+    int sector = atoi(argv[5]);
 
+    ofstream archivo(nombreArchivo);
+
+    if (!archivo) {
+        cerr << "No se pudo abrir el archivo." << endl;
+        return 1;
+    }
+
+    srand(static_cast<unsigned>(time(0)));
+
+    int caracteresEscritos = 0;
+    int sectorInicio = 0;
+    int sectorFin = 0;
+
+    while (caracteresEscritos < longitudDeseada) {
+        int longitudLinea = rand() % 101 + 100; // Entre 100 y 200 caracteres
+        if (longitudDeseada - caracteresEscritos < 100) {
+            longitudLinea = longitudDeseada - caracteresEscritos;
+        }
+
+        // Calcula el inicio y fin del sector actual
+        sectorInicio = caracteresEscritos;
+        sectorFin = sectorInicio + longitudLinea;
+
+        // Inserta el patrón en una posición aleatoria dentro del sector
+        if (sectorFin < longitudDeseada) {
+            int posicionInsercion = sectorInicio + rand() % (longitudLinea + 1);
+            for (int i = sectorInicio; i < sectorFin; i++) {
+                if (i == posicionInsercion) {
+                    archivo << "patron"; // Inserta el patrón
+                }
+                char caracter;
+                int tipoCaracter = rand() % 3; // 0: Mayúscula, 1: Minúscula, 2: Número
+                if (tipoCaracter == 0) {
+                    caracter = 'A' + rand() % 26; // Mayúscula
+                } else if (tipoCaracter == 1) {
+                    caracter = 'a' + rand() % 26; // Minúscula
+                } else {
+                    caracter = '0' + rand() % 10; // Número
+                }
+                archivo << caracter;
+            }
+            if (posicionInsercion == sectorFin) {
+                archivo << "patron"; // Inserta el patrón al final del sector
+            }
+        } else {
+            // Si el sector se desborda, simplemente agrega caracteres sin inserción de patrón
+            for (int i = sectorInicio; i < longitudDeseada; i++) {
+                char caracter;
+                int tipoCaracter = rand() % 3; // 0: Mayúscula, 1: Minúscula, 2: Número
+                if (tipoCaracter == 0) {
+                    caracter = 'A' + rand() % 26; // Mayúscula
+                } else if (tipoCaracter == 1) {
+                    caracter = 'a' + rand() % 26; // Minúscula
+                } else {
+                    caracter = '0' + rand() % 10; // Número
+                }
+                archivo << caracter;
+            }
+        }
+
+        archivo << endl; // Agregar un salto de línea
+        caracteresEscritos += longitudLinea;
+    }
+
+    archivo.close();
+    if (sector < 1 || sector > 10) {
+        cerr << "El valor de 'sector' debe estar en el rango de 1 a 10." << endl;
+        return 1;
+    }
+
+    ifstream archivoEntrada(nombreArchivo);
+
+    if (!archivoEntrada) {
+        cerr << "No se pudo abrir el archivo de entrada." << endl;
+        return 1;
+    }
+
+    string contenidoArchivo((istreambuf_iterator<char>(archivoEntrada)), (istreambuf_iterator<char>()));
+    archivoEntrada.close();
+
+    int totalCaracteres = contenidoArchivo.size();
+    int inicioSector = ((sector - 1) * totalCaracteres) / 10;
+    int finSector = (sector * totalCaracteres) / 10;
+
+    string nuevoContenido = contenidoArchivo.substr(0, inicioSector);
+
+    for (int i = 0; i < repeticiones; i++) {
+        nuevoContenido += patronAInsertar;
+    }
+
+    nuevoContenido += contenidoArchivo.substr(inicioSector, finSector - inicioSector);
+    nuevoContenido += contenidoArchivo.substr(finSector);
+
+    ofstream archivoSalida(nombreArchivo);
+
+    if (!archivoSalida) {
+        cerr << "No se pudo abrir el archivo de salida." << endl;
+        return 1;
+    }
+
+    archivoSalida << nuevoContenido;
+    archivoSalida.close();
+
+    cout << "El archivo se ha modificado exitosamente." << endl;
+    
     return 0;
 }
