0918ce2444090342c71554b4cfb73c8bc048015c
Vicente Cser <vicentecser@gmail.com>
2023-11-06 13:57:07 -0300

    Clean fmindex code

--- fm_index.cpp
+++ fm_index.cpp
@@ -11,13 +11,9 @@ class FMIndexSearch {
 private:
     string index_suffix = ".fm9";
     csa_wt<wt_huff<rrr_vector<127> >, 512, 1024> fm_index;
-    size_t max_locations;
-    size_t post_context;
-    size_t pre_context;
 
 public:
-    FMIndexSearch(size_t max_loc=5, size_t post_ctx=10, size_t pre_ctx=10)
-        : max_locations(max_loc), post_context(post_ctx), pre_context(pre_ctx) {}
+    FMIndexSearch() {}
 
     bool constructIndex(const string& file) {
         string index_file = file + index_suffix;
@@ -37,79 +33,28 @@ public:
         return true;
     }
 
-    
-
-    void search() {
-        std::string primeraLinea;
-        std::ifstream archivo("patron.txt");
-        if (archivo.is_open()) {
-        std::getline(archivo, primeraLinea);
-        archivo.close();
-        } else {
-        // Manejar el caso en que no se pueda abrir el archivo
-        std::cerr << "No se pudo abrir el archivo: " << std::endl;
+    int count(string T, string p) {
+        if (constructIndex(T)) {
+            return displayOccurrences(p);
         }
-        //cout << "Index construction complete, index requires " << size_in_mega_bytes(fm_index) << " MiB." << endl;
-        //out << "Input search terms and press Ctrl-D to exit." << endl;
-        //string prompt = "\e[0;32m>\e[0m ";
-        //cout << prompt;
-        string query =primeraLinea;
-        displayOccurrences(query);
-        //cout << endl;
+        return -1;
     }
 
-    void displayOccurrences(const string& query) {
+    int displayOccurrences(const string& query) {
         size_t m = query.size();
         size_t occs = sdsl::count(fm_index, query.begin(), query.end());
-        //cout << "# of occurrences: " << occs << endl;
-        /*
-        if (occs > 0) {
-            cout << "Location and context of first occurrences: " << endl;
-            auto locations = locate(fm_index, query.begin(), query.begin() + m);
-            sort(locations.begin(), locations.end());
-            for (size_t i = 0, pre_extract = pre_context, post_extract = post_context; i < min(occs, max_locations); ++i) {
-                cout << setw(8) << locations[i] << ": ";
-                if (pre_extract > locations[i]) {
-                    pre_extract = locations[i];
-                }
-                if (locations[i] + m + post_extract > fm_index.size()) {
-                    post_extract = fm_index.size() - locations[i] - m;
-                }
-                auto s = extract(fm_index, locations[i] - pre_extract, locations[i] + m + post_extract - 1);
-                string pre = s.substr(0, pre_extract);
-                s = s.substr(pre_extract);
-                if (pre.find_last_of('\n') != string::npos) {
-                    pre = pre.substr(pre.find_last_of('\n') + 1);
-                }
-                cout << pre;
-                cout << "\e[1;31m";
-                cout << s.substr(0, m);
-                cout << "\e[0m";
-                string context = s.substr(m);
-                cout << context.substr(0, context.find_first_of('\n')) << endl;
-            }
-        }
-        */
+        return occs;
     }
 };
 
 int main(int argc, char** argv) {
     if (argc < 2) {
-        cout << "Usage " << argv[0] << " text_file [max_locations] [post_context] [pre_context]" << endl;
+        cout << "Usage " << argv[0] << " <text_file> <pattern>" << endl;
         return 1;
     }
 
-    size_t max_locations = 5;
-    size_t post_context = 10;
-    size_t pre_context = 10;
-    if (argc >= 3) max_locations = atoi(argv[2]);
-    if (argc >= 4) post_context = atoi(argv[3]);
-    if (argc >= 5) pre_context = atoi(argv[4]);
-
-    FMIndexSearch fmSearch(max_locations, post_context, pre_context);
-    if (fmSearch.constructIndex(argv[1])) {
-        fmSearch.search();
-    }
+    FMIndexSearch fmSearch;
+    fmSearch.count(argv[1], argv[2]);
 
     return 0;
 }
